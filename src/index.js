25c4b390fd210f97160384b9dc07ab43fa85bc36
Ben <benjamin.j.brooks@gmail.com>
2017-09-09 17:48:56 -0700

    initial commit
    getting Cognitration v1.0 into source control.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Cognitration from './Cognitration';
+import './index.css';
+
+ReactDOM.render(<Cognitration />, document.getElementById('cognitration-root'));
+
